fc95223653ee33336c1d9e6df9e8eb04439f7844
Aya <Sherbnya@gmail.com>
2025-12-02 20:55:15 +0000

    Create battle manager

---
+++ 00_autoloads/00_battle_manager.gd
@@ -0,0 +1,40 @@
+extends Node
+
+var current_demon: Demon
+var demon_hp: int
+var demon_seduction: int
+
+enum STATES {
+	NOT_IN_BATTLE,
+	AWAITING_TURN_CHOICE,
+	EXECUTING_TURNS,
+	BATTLE_OVER,
+}
+
+var battle_state: STATES
+var turn_queue: Array = []
+
+func _ready() -> void:
+	battle_state = STATES.NOT_IN_BATTLE
+
+func initiate_fight() -> void:
+	_set_up_demon()
+	battle_state = STATES.AWAITING_TURN_CHOICE
+
+func execute_turn() -> void:
+	battle_state = STATES.EXECUTING_TURNS
+	for turn in turn_queue:
+		if turn is Item:
+			turn.effect.trigger()
+	finish_turn()
+
+func finish_turn() -> void:
+	# hp/seduction checks in to battleover state
+	battle_state = STATES.AWAITING_TURN_CHOICE
+
+func _set_up_demon() -> void:
+	demon_hp = current_demon.hp
+	demon_seduction = 0
+
+func leave_battle() -> void:
+	battle_state = STATES.NOT_IN_BATTLE
--- resources/demon.gd
+++ resources/demon.gd
@@ -1,1 +1,14 @@
 class_name Demon extends Resource
+
+@export_category("Assets")
+@export var image: Texture2D
+
+@export_category("Stats")
+@export var hp: int
+@export var seduction_target: int
+
+@export_category("Dialogue")
+@export_multiline var entry_text: String
+@export_multiline var beaten_text: String
+@export_multiline var seduced_text: String
+@export_multiline var lose_text: String
