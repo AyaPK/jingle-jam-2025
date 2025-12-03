5745e06f00fd879fc2d8b07ea4a8704cabe14a92
Olly Larkin <olly.larkin@softwire.com>
2025-12-03 20:07:43 +0000

    Wrath image updates

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,5 @@
 extends Node2D
-@onready var button: Button = $CanvasLayer/HFlowContainer/Button
+@onready var button: Button = $CanvasLayer/HFlowContainer/WrathButton
 @onready var partners_button: Button = $CanvasLayer/HBoxContainer/PartnersButton
 
 func _ready() -> void:
